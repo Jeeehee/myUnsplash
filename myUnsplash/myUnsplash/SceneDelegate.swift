19870ab3f00e42d8a27ffd5e8a57485b95eecec7
Dihee <hwangjihee94@gmail.com>
2022-12-09 00:27:38 +0900

    chore: Create Project

---
+++ myUnsplash/myUnsplash/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  myUnsplash
+//
+//  Created by Jihee hwang on 2022/12/09.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ myUnsplash/myUnsplash/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myUnsplash
+//
+//  Created by Jihee hwang on 2022/12/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+    }
+
+
+}
+
